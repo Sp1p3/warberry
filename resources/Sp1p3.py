4f1af3fe2360518e27f540ce9b7b17517de30321
Sp <>
2016-06-01 13:37:11 +0200

    Addition of the personnal module Sp1p3.py in ressources with a small disclamer in french

---
+++ resources/Sp1p3.py
@@ -0,0 +1,9 @@
+#coding=utf-8
+#Modification de fonction et ajout de fonctionnalité au Warberry de SecGroundZero
+#Et je fais le disclamer en Fr si je veux 
+#Les modules additionnels sont sous licence RACH
+#Et fourni dans l'état je suis pas responsable de tout ce que vous pourrez faire avec 
+
+
+
+__author__ = "Sp1p3"
